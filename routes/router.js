116470173359cce90aa5bd35f705f8e46a3939a7
Austin Eng <213reeses@gmail.com>
2015-11-13 23:36:33 -0500

    authentication checking

--- routes/router.js
+++ routes/router.js
@@ -4,4 +4,16 @@ var router = express.Router();
 
 module.exports = router;
 
+router.isAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+  res.redirect('/login');
+}
+
+router.get('/', router.isAuthenticated, function (req, res) {
+  res.render('index', {
+  	
+  });
+});
+
 require('./account')(router);
